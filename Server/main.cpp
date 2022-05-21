90efd56e586da02ad1795773c06a327386881444
wesslll <ovcharenko.nikita.sergeyevich@gmail.com>
2022-05-21 20:38:48 +0600

    Initial Commit

---
+++ Server/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Server
+//
+//  Created by Nikita on 5/20/22.
+//
+
+#include <iostream>
+#include <stdio.h>
+#define max(A,B) ((A)>(B) ? (A) : (B))
+int main()
+{
+    printf("Max=%d\n",max(8,9));
+}
